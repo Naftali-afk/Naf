6b9aa9d673be3c998ecd8ce084eb8e0044d49e74
Naftali-afk <naftaliloudasilva2017@gmail.com>
2020-08-30 23:34:12 -0300

    Atualização nova

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ console.log("Olá Mundo dsada");
 function Test(a,b){
     return a + b;
 }
+
 $('p').click(function (e) { 
     e.preventDefault();
     
 });
+
+
+/* Comitar as alterações;
+   Git pull;
+   Corrigir conflitos;
+   comitar sem conflitos;
+   git push;
+*/
